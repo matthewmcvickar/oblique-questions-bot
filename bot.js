d8f4afd8d242f5738d8909abb78c046cca445e57
Matthew McVickar <matthew@matthewmcvickar.com>
2018-11-16 11:33:18 -0800

    Make the bot also post to Mastodon.

--- bot.js
+++ bot.js
@@ -1,46 +1,103 @@
 'use strict';
 
+// Change this to `true` to post to the web
+// when you run this script locally.
+var canPostFromLocal = false;
+
+// Load libraries.
 var Twit = require('twit');
-var T = new Twit({
-  consumer_key:        process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret:     process.env.TWITTER_CONSUMER_SECRET,
-  access_token:        process.env.TWITTER_ACCESS_TOKEN,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-});
+var Masto = require('mastodon');
 
-// When tweeting from local machine.
-// var T = new Twit(require('./config.js'));
+// Are we on production? Check if an important environment variable exists.
+function isProduction () {
+  return (
+    typeof(process.env.MASTODON_ACCESS_TOKEN) !== 'undefined' ||
+    typeof(process.env.TWITTER_CONSUMER_KEY) !== 'undefined'
+  );
+}
 
+// Use environment variables if we're on production, config files if we're local.
+if (isProduction()) {
+  var twitter = new Twit({
+    consumer_key:        process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret:     process.env.TWITTER_CONSUMER_SECRET,
+    access_token:        process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  });
+
+  var mastodon = new Masto({
+    access_token: process.env.MASTODON_ACCESS_TOKEN,
+  });
+}
+else {
+  var mastodon = new Masto(require('./config/mastodon-config.js'));
+  var twitter  = new Twit(require('./config/twitter-config.js'));
+}
+
+// Get a question and post it!
 var questions         = require('./data/questions-corpus.json');
 var numberOfQuestions = Object.keys(questions).length;
 
-function getQuestion() {
-  return questions[Math.floor(Math.random()*numberOfQuestions)];
+// Execute and post!
+createAndPost();
+
+// The main process. Get a useable comment and tweet it or try again.
+function createAndPost () {
+  var question = getQuestion();
+  return post(question);
 }
 
-function tweet() {
-  var theQuestion = getQuestion();
+// Get a random question.
+function getQuestion () {
+  return questions[Math.floor(Math.random()*numberOfQuestions)].toString();
+}
 
-  T.post('statuses/update', { status: theQuestion }, function(err) {
-    if (err) {
-      console.log('error:', err);
+// Post to the web.
+function post (data) {
+  if (typeof(data) !== 'undefined') {
+    console.log('NOW ATTEMPTING TO POST:', data);
+
+    if (isProduction() || canPostFromLocal) {
+      // Twitter.
+      twitter.post('statuses/update', { status: data }, function (error) {
+        if (error) {
+          console.log('ERROR POSTING:', error);
+        }
+        else {
+          console.log('SUCCESS!');
+        }
+      });
+
+      // Mastodon.
+      mastodon.post('statuses', { status: data }, function (error) {
+        if (error) {
+          console.log('ERROR POSTING:', error);
+        }
+        else {
+          console.log('SUCCESS!');
+        }
+      });
     }
-  });
+  }
+  else {
+    console.log('ERROR: No post created; cannot post.');
+  }
 }
 
-// Tweet on a regular schedule.
-var timesToTweetPerDay = 6;
+// Note that because Heroku machines restart at least once per day, the bot's
+// schedule will not be regular:
+// https://devcenter.heroku.com/articles/how-heroku-works#dyno-manager
+if (isProduction()) {
+  var millisecondsInDay = 1000 * 60 * 60 * 24;
+  var timesToPostPerDay = 6;
+  var postInterval = millisecondsInDay / timesToPostPerDay;
 
-// Because Heroku cycles dynos once per day, the bot's schedule will not be
-// regular: https://devcenter.heroku.com/articles/how-heroku-works#dyno-manager
-setInterval(function () {
-  try {
-    tweet();
-  }
-  catch (e) {
-    console.log(e);
-  }
-}, (1000 * 60 * 60 * 24) / timesToTweetPerDay);
-
-// Tweet once on initialization.
-tweet();
+  setInterval(function () {
+    try {
+      createAndPost();
+    }
+    catch (error) {
+      console.log('PROCESS UNSUCCESSFUL!', error);
+    }
+  }, postInterval);
+}
