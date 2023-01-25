95ae1f6fea5429fdbd04a1e48f1d3d24abc3eb8a
Matthew McVickar <matthew@matthewmcvickar.com>
2023-01-25 09:54:58 -0800

    add example config files

---
+++ config/mastodon-config.example.js
@@ -0,0 +1,4 @@
+module.exports = {
+  api_url: 'https://botsin.space/api/v1/',
+  access_token: '12345',
+};
---
+++ config/twitter-config.example.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:        '1234',
+  consumer_secret:     '5789',
+  access_token:        '0123',
+  access_token_secret: '4567'
+};
